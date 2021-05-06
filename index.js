f698b334365e4993c44b2b0b7ee1ba41395b7540
Your Name <you@example.com>
2021-05-06 18:52:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+// multiply
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+
+// random
+let random = Math.ceil(Math.random()) // *1
+
+//mod
+let num3 = 48
+let num4 = 22
+let mod = num3 % num4
+
+//max
+let max = Math.max(20, 15, 14, 9, 16, 4)
